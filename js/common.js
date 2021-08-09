c3357da32230d8fcd8b08285f306e100049d0822
hannah3406 <86402077+hannah3406@users.noreply.github.com>
2021-08-09 11:00:12 +0900

    20210809

---
+++ js/common.js
@@ -0,0 +1,49 @@
+//inc.html
+$(function(){
+	$('header').load('inc.html .head-all',head);
+	$('footer').load('inc.html .footer',);
+});
+
+function head(){
+//menu_box slideUp/Down
+//1)전체hide
+$('.menu_box').hide();
+$('.menu_box_content').hide();
+
+$('.menu_list ul li').mouseenter(function(){
+    var idx = $(this).index();
+
+    if(idx > 1 && idx <5){
+        //2) 3번째 메뉴부터 5번째 메뉴까지
+        $('.menu_box').stop().slideDown(500);
+        $('.menu_box_content').hide();
+        $('.menu_box_content').eq(idx-1).show();
+    }else if(idx == 0){
+        //3) 1번째 메뉴
+        $('.menu_box').stop().slideDown(500);
+        $('.menu_box_content').hide();
+        $('.menu_box_content').eq(idx).show();
+    }else{
+        //4) 그 외 메뉴 클릭시 slideUp
+        $('.menu_box').slideUp();
+    }
+})
+//slideUp
+$('.menu_box').mouseleave(function(){
+    $(this).stop().slideUp(500);
+})
+
+
+//ham_menu click event
+const MenuTrigger = document.querySelector('.menu_trigger');
+const HamMenu = document.querySelector('.ham_menu');
+const HamClose = document.querySelector('.ham_close');
+
+    MenuTrigger.addEventListener('click',function(){
+        HamMenu.classList.add('active');
+    });
+    HamClose.addEventListener('click',function(){
+        HamMenu.classList.remove('active');
+    })
+
+}
--- js/index.js
+++ js/index.js
@@ -1,145 +1,102 @@
-//menu_box slideUp/Down
-//1)전체hide
-$('.menu_box').hide();
-$('.menu_box_content').hide();
+//json 파일 연결하기
 
-$('.menu_list ul li').mouseenter(function(){
-    var idx = $(this).index();
+fetch('js/prdList.json')
+    .then( (res) => res.json() )
+    .then( (data) => callback(data) );
 
-    if(idx > 1 && idx <5){
-        //2) 3번째 메뉴부터 5번째 메뉴까지
-        $('.menu_box').stop().slideDown(500);
-        $('.menu_box_content').hide();
-        $('.menu_box_content').eq(idx-1).show();
-    }else if(idx == 0){
-        //3) 1번째 메뉴
-        $('.menu_box').stop().slideDown(500);
-        $('.menu_box_content').hide();
-        $('.menu_box_content').eq(idx).show();
-    }else{
-        //4) 그 외 메뉴 클릭시 slideUp
-        $('.menu_box').slideUp();
+    function callback(data){
+        //section2의 Tab 부분
+        //prd-list 선택자 잡아주기
+        const Slider = document.querySelectorAll('.center');
+        let msg = ['','','',''],srcFront,srcBack,tit,detail;
+
+        for(let i=0;i<Slider.length;i++){
+
+            data.items[i].good.forEach(function(v,k){
+                srcFront = data.items[i].good[k].srcFront
+                srcBack = data.items[i].good[k].srcBack
+                tit = data.items[i].good[k].tit
+                detail = data.items[i].good[k].detail
+
+                msg[i] += `                    
+                <div class="item-content">
+                    <figure class=photo>
+                        <img src="${srcFront}" alt="m-2">
+                        <img src="${srcBack}" alt="m-2">
+                    </figure>
+                    <div class="item-content-text">
+                        <strong>${tit}</strong>
+                        <span>${detail}</span>
+                    </div>
+                </div>
+                `
+            });
+
+            //Slider[i].innerHTML = msg;
+            Slider[i].innerHTML = msg[i];
+        }
+
+
+    //slick
+        //m-2
+        $(".center").slick({
+            dots: false,
+            infinite: true,
+            centerMode: false,
+            slidesToShow: 4.5,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                  breakpoint: 1600,
+                  settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                  }
+                }
+            ]
+        });
+        
+        if(window.matchMedia("(min-width: 1400px)").matches){
+            $(".center").height( $(".item-content").height(400) )
+        } else{
+            $(".center").height( $(".item-content").height(350) )
+        }
+
+
+
+        //m-3
+        $(".m-3-center").slick({
+            dots: false,
+            infinite: true,
+            centerMode: false,
+            slidesToShow: 3,
+            slidesToScroll: 1
+        });
+        //m-6
+        $(".m-6-center").slick({
+            dots: false,
+            infinite: true,
+            centerMode: false,
+            slidesToShow: 4,
+            slidesToScroll: 1
+        });
+
+    //section2 의 Tab기능
+    const btnLi = document.querySelectorAll('.prd-tab li'); //클릭할 것
+    const elPrdList = document.querySelectorAll('.prd-list'); //보여줄 것
+    let num = 0; //선택한 index No와 일치시키기 위한 변수 생성
+
+    for(let i=0;i<btnLi.length;i++){
+        btnLi[i].addEventListener('click',function(){
+            btnLi[num].classList.remove('on');
+            elPrdList[num].classList.remove('on');
+            btnLi[i].classList.add('on');
+            elPrdList[i].classList.add('on');
+            num = i;
+
+        })
     }
-})
-//slideUp
-$('.menu_box').mouseleave(function(){
-    $(this).stop().slideUp(500);
-})
 
-
-//ham_menu click event
-const MenuTrigger = document.querySelector('.menu_trigger');
-const HamMenu = document.querySelector('.ham_menu');
-const HamClose = document.querySelector('.ham_close');
-
-    MenuTrigger.addEventListener('click',function(){
-        HamMenu.classList.add('active');
-    });
-    HamClose.addEventListener('click',function(){
-        HamMenu.classList.remove('active');
-    })
-
-
-//slick
-if (window.matchMedia("(min-width: 1600px)").matches) {
-    //m-2
-    $(".center").slick({
-        dots: false,
-        infinite: true,
-        centerMode: false,
-        slidesToShow: 4.5,
-        slidesToScroll: 1
-    });
-    //m-3
-    $(".m-3-center").slick({
-        dots: false,
-        infinite: true,
-        centerMode: false,
-        slidesToShow: 3,
-        slidesToScroll: 1
-    });
-    //m-6
-    $(".m-6-center").slick({
-        dots: false,
-        infinite: true,
-        centerMode: false,
-        slidesToShow: 4,
-        slidesToScroll: 1
-    });
-}else if(window.matchMedia("(min-width: 1400px)").matches) {    
-    //m-2    
-    $(".center").slick({
-    dots: false,
-    infinite: true,
-    centerMode: false,
-    slidesToShow: 4,
-    slidesToScroll: 1
-    });
-    //m-3
-    $(".m-3-center").slick({
-        dots: false,
-        infinite: true,
-        centerMode: false,
-        slidesToShow:4,
-        slidesToScroll: 1
-    });
-    //m-6
-    $(".m-6-center").slick({
-        dots: false,
-        infinite: true,
-        centerMode: false,
-        slidesToShow: 4,
-        slidesToScroll: 1
-    });
-}else if(window.matchMedia("(min-width: 1200px)").matches){
-    //m-2
-    $(".center").slick({
-    dots: false,
-    infinite: true,
-    centerMode: false,
-    slidesToShow: 4,
-    slidesToScroll: 1
-    });
-    //m-3
-    $(".m-3-center").slick({
-        dots: false,
-        infinite: true,
-        centerMode: false,
-        slidesToShow: 3,
-        slidesToScroll: 1
-    });
-    //m-6
-    $(".m-6-center").slick({
-        dots: false,
-        infinite: true,
-        centerMode: false,
-        slidesToShow: 4,
-        slidesToScroll: 1
-    });
 }
-else{
-    //m-2
-    $(".center").slick({
-    dots: false,
-    infinite: true,
-    centerMode: false,
-    slidesToShow: 4,
-    slidesToScroll: 1
-    });
-    //m-3
-    $(".m-3-center").slick({
-        dots: false,
-        infinite: true,
-        centerMode: false,
-        slidesToShow: 3,
-        slidesToScroll: 1
-    });
-    //m-6
-    $(".m-6-center").slick({
-        dots: false,
-        infinite: true,
-        centerMode: false,
-        slidesToShow: 4,
-        slidesToScroll: 1
-    });
-}
+
+
