f9cccc7e6cf6e12f20f05b886c31f81a81b531d7
hannah3406 <86402077+hannah3406@users.noreply.github.com>
2021-08-11 18:16:32 +0900

    20210811

--- js/sub.js
+++ js/sub.js
@@ -1,4 +1,21 @@
 //mouse wheel event
+//스크롤 내리면 section하나씩 등장/퇴장
+window.addEventListener('scroll',function(){
+    const elM = document.querySelectorAll('.elM');
+    let elHei = window.innerHeight;
+    let num = 100;
+
+    for(let i=0;i<elM.length;i++){
+        if(elHei <= window.scrollY* num){
+            elM[i].classList.add('active');
+        }else{
+            elM[i].classList.remove('active');
+        }
+        num ++;
+}
+
+})
+//이미지 사이즈 크기 확대/축소
 const imgZoom = document.querySelectorAll('.imgZoom');
 const zoomSpeed = 0.001;
 let zoom = 0.01;
@@ -14,3 +31,4 @@ imgZoom.forEach(function(v,k){
         imgZoom[k].style.transform = `scale(${zoom})`;
     });
 })
+
