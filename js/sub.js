cb43220a3369812ae8fe608bf34a5b7b510e4fbd
hannah3406 <86402077+hannah3406@users.noreply.github.com>
2021-08-13 09:23:24 +0900

    20210813

--- js/sub.js
+++ js/sub.js
@@ -1,39 +1,58 @@
-//mouse wheel event
-//스크롤 내리면 section하나씩 등장/퇴장
-window.addEventListener('scroll',function(){
-    const elM = document.querySelectorAll('.elM');
-    let num = 0; 
+const elM = document.querySelectorAll('.elM');
+let obj=[], pos={y:0,y2:0,state:true};
+//objani 함수
+function objani(el){
+    this.imgZoom = el.querySelectorAll('.imgZoom');
+    this.zoomSpeed = 0.001;
+    this.zoom = 0.5;
+    this.rotateImg = 3.5;
+    this.rotateImgSpeed = 0.2;
+
+    this.update = function(){   
+        try{
+            if(pos.state){
+                this.zoom += this.zoomSpeed;
+                this.rotateImg += this.rotateImgSpeed;
+            }else{
+                this.zoom -= this.zoomSpeed;
+                this.rotateImg -= this.rotateImgSpeed;
+            }
+            for(let i=0;i<this.imgZoom.length;i++){
+                this.imgZoom[i].style.transform = `scale(${this.zoom}) rotate(${this.rotateImg}deg)`;
+            }
+        }catch{}
+    }        
+}
+//objani 함수선언(3번반복문)
+for(let i=0;i<elM.length;i++){
+    obj.push(new objani(elM[i]) )   
+}
+//스크롤 아래로 내리면 false, 위로 올리면 true
+function scrollState(){
+    pos.y = window.scrollY;
+    (pos.y > pos.y2) ? pos.state=true : pos.state=false; 
+    pos.y2 = pos.y;
+}
+//스크롤 이벤트
+window.addEventListener('scroll',function(){ 
+    scrollState();   
+    let num = 0;     
 
     for(let i=0; i<elM.length; i++){
-        let elHei = elM[i].offsetTop - elM[i].offsetHeight;
-        let winHei = window.innerHeight;
+        let elHei = elM[i].offsetTop - 80;
 
-        if(elHei-winHei <= window.scrollY){
+        if(elHei <= window.scrollY){
             elM[num].classList.remove('active');
             elM[i].classList.add('active');
+            obj[i].update();
         }else{
             elM[i].classList.remove('active');
         }  
         num = i;
     }
 })
-//이미지 사이즈 크기 확대/축소
-for(let i=0;i<3;i++){
-    const imgZoom = document.querySelectorAll('.imgZoom' + i);
-    const zoomSpeed = 0.0005;
-    let zoom = 0.5;
 
-    imgZoom.forEach(function(v,k){
-        document.addEventListener("wheel", function(e){
-            if(e.deltaY > 0){
-                zoom += zoomSpeed;
-            }else{
-                zoom -= zoomSpeed;
-            }
-            imgZoom[k].style.transform = `scale(${zoom})`;
-        });
-    })
-}
+
 
 //사진 회전하면서 옆으로 움직이기
     const Photo = document.querySelectorAll('.s-3-photo img');
@@ -49,13 +68,14 @@ for(let i=0;i<3;i++){
 
     Photo.forEach(function(v,k){
         document.addEventListener("wheel", function(e){
-            if(e.deltaY > 0 ){
-                // Photo[k].style.animation = `rotateS3 1.5s infinite`
-                Photo[k].style.transform = `rotate(2deg) translateY(-50%)`
+            if(e.deltaY > 0){
+                Photo[k].style.transform +=`rotate(1deg)`;
+                // Photo[2*k].style.transform +=`rotate(-3deg)`;
             }else{
-                // Photo[k].style.transform = `rotateS3 1.5s infinite`
-                Photo[k].style.transform = `rotate(-2deg) translateY(-50%)`
+                // Photo[k].style.transform +=`rotate(-3deg)`;
+                Photo[k].style.transform +=`rotate(-1deg)`;
             }
+            Photo[k].style.transform = `translateY(-50%)`;
         });
     })
 
