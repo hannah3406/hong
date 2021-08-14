539ab5d23958683cc266e88d899fca4bbd5ead92
hannah3406 <86402077+hannah3406@users.noreply.github.com>
2021-08-14 23:21:25 +0900

    20210814

---
+++ js/join_level_1.js
@@ -0,0 +1,24 @@
+const impMsg1 = document.querySelector('.important_check_msg1')
+const impMsg2 = document.querySelector('.important_check_msg2')
+//전체동의 버튼
+contentAll.addEventListener('click',function(){
+        content1.checked = contentAll.checked
+        content2.checked = contentAll.checked
+})
+
+//next버튼 click
+btnNextStep.addEventListener('click',function(){
+        if(!content1.checked){
+                impMsg1.classList.add('active');
+        }else if(!content2.checked){
+                impMsg2.classList.add('active');
+        }else{
+        join.action = 'join.html';
+        join.submit();
+        }
+})
+//prev버튼 click
+btnPrevStep.addEventListener('click',function(){
+        join.action = 'join.html';
+        join.submit();
+})
